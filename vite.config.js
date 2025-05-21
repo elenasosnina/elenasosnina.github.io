b64be33d8de51aaa2460c2f658edcd576b5b744f
elenasosnina <tatalenakot@gmail.com>
2025-05-21 18:23:46 +0300

    8

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {  
-   headers: {
-     'Content-Type': 'application/javascript', 
-   }
-  },
+  plugins: [react()]
 })
